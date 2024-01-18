780fa34bfaaad6796001dd571efd5b84584ab854
Robert Bossy <Robert.Bossy@inra.fr>
2024-01-18 07:10:07 +0100

    updated prediction data

--- 1-assessment/deferred/dev/Lives_In/config.sh
+++ 1-assessment/deferred/dev/Lives_In/config.sh
@@ -1,7 +1,7 @@
 OUTPUT_DIR="output"
 CONDA_EXECUTABLE="conda"
 CONDA_ENVIRONMENT="null"
-FINETUNED_MODEL="/home/rbossy/code/omnicrobe/text-mining-workflow/ancillaries/re-bert/finetuned_model"
+FINETUNED_MODEL="/home/rbossy/code/re-bert/finetuned_model"
 DATA_FILE="input.csv"
 REBERT_DIR="/home/rbossy/code/re-bert"
 FORCE_CPU="--force_cpu"
