db7debe1addf6311c6a792cbf7d4d2bd6ddc8fa0
Robert Bossy <Robert.Bossy@inra.fr>
2024-11-22 12:10:39 +0100

    added training

---
+++ train.sh
@@ -0,0 +1,16 @@
+#!/bin/env bash
+
+#SBATCH --job-name=anlp-test
+#SBATCH --output=run.out
+#SBATCH --error=run.err
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --partition=testing
+#SBATCH --qos=debug
+
+ALVISNLP=/mnt/beegfs/projects/alvisnlp/src/alvisnlp/.test/alvisnlp/bin/alvisnlp-0.11.0-SNAPSHOT
+
+SET=$1
+DATA_DIR=0-data/BioNLP-OST-2019_BB-rel+ner/$SET
+
+srun $ALVISNLP -inputDir $DATA_DIR -inputDir . -outputDir $DATA_DIR alvisnlp/train.plan
